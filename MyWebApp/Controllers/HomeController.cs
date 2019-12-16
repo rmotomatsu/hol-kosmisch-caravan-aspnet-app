b1187e14c635c8a4164cd51a05c83e847c19ad41
unknown <r-motomatsu@ASCA.local>
2019-12-16 16:49:38 +0900

    ASP.NET Coreに移行した

--- MyWebApp/Controllers/HomeController.cs
+++ MyWebApp/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MyWebApp.Models;
-using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 namespace MyWebApp.Controllers
 {
@@ -7,18 +8,15 @@ namespace MyWebApp.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = Session["message"]?.ToString() ?? "";
-            return View();
+            string message = HttpContext.Session.GetString("message");
+            return View(new MyForm { Message = message });
         }
 
         [HttpPost]
         public ActionResult Index(MyForm item)
         {
-            if (!string.IsNullOrWhiteSpace(item?.Message))
-            {
-                Session["message"] = item.Message;
-            }
+            HttpContext.Session.SetString("message", item.Message);
             return RedirectToAction("Index");
         }
     }
-}
+    
